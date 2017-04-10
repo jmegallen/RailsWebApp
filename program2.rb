f19d9d8eb3ce35552e833388f9847034e46e8747
Jamie Gallen <jme@gmail.com>
2017-04-10 14:44:10 +0200

    Ruby programs 1-4
    Learning Ruby
    3.2: Control Flow in Ruby

---
+++ program2.rb
@@ -0,0 +1,5 @@
+if (5+5==10)
+  puts "this is true"
+else
+  puts "this is false"
+end
---
+++ program3.rb
@@ -0,0 +1,15 @@
+def choose
+  puts "Do you like programming? Yes, no, or maybe please."
+  choice = gets.chomp
+  case choice.downcase
+  when "yes"
+    puts "That\'s great!"
+  when "no"
+    puts "That\'s too bad!"
+  when "maybe"
+    puts "Glad you are giving it a chance!"
+  else
+    puts "I have no idea what that means."
+  end
+end
+choose
