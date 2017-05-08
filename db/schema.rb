61fb0c11ef40b68fde71c15cb3a3ca009e1a4de2
Jamie Gallen <jme@gmail.com>
2017-05-08 16:24:55 +0200

    Rolled back again to change coluor name
    Also did some tidying of the box model for the bikes pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504133627) do
+ActiveRecord::Schema.define(version: 20170508134830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170504133627) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
     t.decimal  "price"
   end
 
