de1aa42c8e8ba2d9e22e91523c2549d33042c194
Jamie Gallen <jme@gmail.com>
2017-05-09 09:21:06 +0200

    Adding orders pages
    routes
    views
    controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,3 +2,12 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+
+
+class StaticPagesController < ApplicationController
+
+  def landing_page
+    @featured_product = Product.first
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'static_pages/about'
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
+
+
 
 end
