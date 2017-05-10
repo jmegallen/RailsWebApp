5f0b98b22358ecfb4d79dacd5c71dcf8e19df0bb
Jamie Gallen <jme@gmail.com>
2017-05-10 16:11:00 +0200

    Create Order Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508134830) do
+ActiveRecord::Schema.define(version: 20170509153750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float   "total"
+    t.index ["product_id"], name: "index_orders_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
