9bd25e61c68f2228033fcc15ac7b6d21c26ea076
Jamie Gallen <jme@gmail.com>
2017-05-13 08:26:26 +0200

    More indentation issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
 
-	def index
-	end
+  def index
+  end
 
-	def landing_page
-		@products = Product.limit(3)
-	end
-	
+  def landing_page
+    @products = Product.limit(3)
+  end
+
 end
