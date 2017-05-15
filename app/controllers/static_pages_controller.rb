651f00f25aef78f843a410af1efa4787d40b60bd
Jamie Gallen <jme@gmail.com>
2017-05-15 17:33:39 +0200

    Configuring Contact page for emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,14 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'your-email@example.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  post 'static_pages/thank_you'
+
+  get 'static_pages/thank_you'
+
   #get '/products/:id', to: 'products#show'
 
   root 'static_pages#landing_page'
