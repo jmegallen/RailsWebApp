c2374af66f90526389252bd642fcc0646b3cd307
Jamie Gallen <jme@gmail.com>
2017-05-15 16:24:12 +0200

    Routes change 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	
-  resources :users
+  #resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
