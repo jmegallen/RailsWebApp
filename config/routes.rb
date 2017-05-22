9bf7d16f59f0eacd6a085b77de2cbc7776130d4d
Jamie Gallen <jme@gmail.com>
2017-05-22 10:06:44 +0200

    Cancan bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
     get "/sign_up" => "users#new", as: "new_user_registration" # custom path to sign_up/registration
   end
-  devise_for :users
+  devise_for :users, :skip => [:registrations]                                          
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
+
 
   resources :users
   #resources :users
