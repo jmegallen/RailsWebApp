a5a6dc5a99ce3b173f774ccf03eb6e1f5d56f44b
Jamie Gallen <jme@gmail.com>
2017-05-23 11:54:25 +0200

    Indent Atom Test - 2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,29 +1,31 @@
 class UserMailer < ApplicationMailer
 	default from: "jmegallen@gmail.com"
 
-  def contact_form(email, name, message)
-  @message = message
-    mail(:from => email,
-         :to => 'jmegallen@gmail.com',
-         :subject => "A new contact form message from #{name}")
-  end
+	def contact_form(email, name, message)
+		@message = message
+		mail(:from => email,
+				 :to => 'jmegallen@gmail.com',
+				 :subject => "A new contact form message from #{name}")
+	end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		UserMailer.contact_form(@email, @name, @message).deliver_now
+	end
 
-  # def welcome_message(email, name)
-  # @message = message
-  #   mail(:from => 'jmegallen@gmail.com',
-  #        :to => email,
-  #        :subject => "Welcome to world of Berlin bikes.....")
+	def welcome_message(email, name)
+	@message = message
+	mail(:from => 'jmegallen@gmail.com',
+			 :to => email,
+		 	 :subject => "Welcome to world of Berlin bikes.....")
+	end
 
-  # def welcome_mailer
-  #   @name = params[:name]
-  #   @email = params[:email]
-  #   UserMailer.welcome_mail(@email, @name).deliver_now
-  # end
+	def welcome_mailer
+		@name = params[:name]
+		@email = params[:email]
+		UserMailer.welcome_mail(@email, @name).deliver_now
+	end
+
 end
