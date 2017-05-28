1b3b50de12de5b1b1c53415af01be5b44226600e
Jamie Gallen <jme@gmail.com>
2017-05-28 18:45:01 +0200

    Comments Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :orders
+  has_many :comments
 
   after_create :welcome_mail
   def welcome_mail
