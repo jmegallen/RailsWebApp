4628d47914831e3beba876b5570f6549bba7d05e
Jamie Gallen <jme@gmail.com>
2017-05-29 04:39:53 +0200

    Fixed First Name issue
    Had to add registrations controller to get the first name to show in
    comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  delegate :first_name, to: :user, allow_nil: true, prefix: true
+
   scope :rating_desc, -> { order(rating: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   has_many :orders
   has_many :comments
 
+  validates :first_name, presence: true 
+  validates :last_name, presence: true
+
   after_create :welcome_mail
   def welcome_mail
     UserMailer.welcome_mail(self).deliver
