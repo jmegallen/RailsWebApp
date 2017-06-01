a716c1a2adbcbf7f753691b2bb99ca6f97763eb7
Jamie Gallen <jme@gmail.com>
2017-06-01 13:19:57 +0200

    Can't make user admin fix -1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
-      can :manage, :all?
-    else
-      can :read, :all?
-    end
-  end
+    can :manage, User, id: user.id
+#     if user.admin?
+#       can :manage, :all?
+#     else
+#       can :read, :all?
+#     end
+#   end
 
-end
+# end
 
 # See the wiki for details:
 # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
