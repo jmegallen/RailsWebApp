889e6d7b650058aa25029b39f2cb03b2b7de3a6e
Jamie Gallen <jme@gmail.com>
2017-06-01 19:46:55 +0200

    Authorise Admin
    Core issue I require first and last names so using console fails due to
    missing names

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    # if user.admin?
-    #   can :manage, :all?
-    # else
-    #   can :read, :all?
-    # end
+    # Temp fix for authorising admins - try to add first and last names 
+    # can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all?
+    else
+      can :read, :all?
+    end
   end
 
  end
