57e8dcded669a43f9219055ed5bafea5e25a7143
Jamie Gallen <jme@gmail.com>
2017-06-03 16:16:40 +0200

    Stipe Test - 7

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,18 +10,17 @@ class PaymentsController < ApplicationController
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
+      customer = Stripe::Customer.create(
+        source: token,
+        receipt_email: @user.email,
+      )
       charge = Stripe::Charge.create(
         amount: (@product.price * 100).to_i, # amount in cents, again
         currency: "eur",
-        source: token,
         # description: "Berlin Bikes",
         # receipt_email: params[:stripeEmail]
-        description: params[:stripeEmail]
+        description: params[:stripeEmail],
       )
-      
-      # customer = Stripe::Customer.create(
-      # receipt_email: @user.email
-      # )
 
     if charge.paid
       Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
