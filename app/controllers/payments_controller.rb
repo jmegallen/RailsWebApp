774e679fcfa79b85956c9e3f1f9433aa7a405e69
Jamie Gallen <jme@gmail.com>
2017-06-03 15:41:00 +0200

    Stipe Test - 6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,12 +16,12 @@ class PaymentsController < ApplicationController
         source: token,
         # description: "Berlin Bikes",
         # receipt_email: params[:stripeEmail]
-        # description: params[:stripeEmail]
+        description: params[:stripeEmail]
       )
       
-      customer = Stripe::Customer.create(
-      receipt_email: @user.email
-      )
+      # customer = Stripe::Customer.create(
+      # receipt_email: @user.email
+      # )
 
     if charge.paid
       Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
