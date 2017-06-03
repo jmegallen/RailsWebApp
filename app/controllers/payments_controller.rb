5be273a5db439e4e9acb13482cef8c7ce04b2d23
Jamie Gallen <jme@gmail.com>
2017-06-03 19:13:54 +0200

    Stipe Test - 9

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -17,8 +17,7 @@ class PaymentsController < ApplicationController
       charge = Stripe::Charge.create(
         :amount => (@product.price * 100).to_i, # amount in cents, again
         :currency => "eur",
-        # description => "Berlin Bikes",
-        # receipt_email  => params[:stripeEmail]
+        :customer => user_id,
         :description => params[:stripeEmail],
       )
 
