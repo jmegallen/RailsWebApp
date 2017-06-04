d7a2f6e79b643cbab83837c53c38dfb076690f60
Jamie Gallen <jme@gmail.com>
2017-06-04 12:15:54 +0200

    Stipe Test - 14

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -14,3 +14,6 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # # Set your secret key: remember to change this to your live secret key in production
 # # See your keys here: https://dashboard.stripe.com/account/apikeys
 # Stripe.api_key = Rails.configuration.stripe[:publishable_key]
+
+# <!--   data-key="<%= Rails.configuration.stripe[:secret_key] %>"
+#   pk_test_TwUTjFTZFArzA5EPS1q14xLA -->
