9def8fa21d10a1b20acf9e4a8e665819231d1935
Jamie Gallen <jme@gmail.com>
2017-06-05 18:42:26 +0200

    6.8: Security - 2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -14,4 +14,3 @@ require "stripe"
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # # Set your secret key: remember to change this to your live secret key in production
 # # See your keys here: https://dashboard.stripe.com/account/apikeys
-
