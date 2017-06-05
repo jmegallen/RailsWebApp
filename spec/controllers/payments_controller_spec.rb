aed8642ab08846ced9438b6b9e9c4d8304a330f2
Jamie Gallen <jme@gmail.com>
2017-06-05 12:36:15 +0200

    Test Updates

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe PaymentsController, type: :controller do
+describe PaymentsController, :type => :controller do
 
+  context 'GET #create' do
+
+    before do
+      get :create
+    end
+
+    it 'responds with an HTTP 302 status code' do
+      expect(response).to have_http_status(302)
+    end
+
+  end  
 end
+
+
