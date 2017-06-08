44493284ea705406bbf41539cde108cd90c22e44
Jamie Gallen <jme@gmail.com>
2017-06-08 12:39:45 +0200

    Path syntax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,12 @@
 var refreshRating = function(){
 
   $('.rating').raty({
-    path: '/assets/images',
+    path: '../assets/images',
     scoreName: 'comment[rating]'
   });
 
   $('.rated').raty({
-    path: '/assets/images',
+    path: '../assets/images',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
