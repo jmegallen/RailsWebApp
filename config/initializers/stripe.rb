9524e23d4cf3e76045d6e77ae5e58752e0b4af3b
Jamie Gallen <jme@gmail.com>
2017-06-08 12:28:32 +0200

    Stripe API Issues
    Hiding keys
    testing key placement

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,8 +5,10 @@ if Rails.env.production?
   }
 else
   Rails.configuration.stripe = {
-    publishable_key: ENV['stripe_publishable_key'],
-    secret_key: ENV['stripe_secret_key']
+    # publishable_key: ENV['stripe_publishable_key'],
+    # secret_key: ENV['stripe_secret_key']
+    publishable_key: 'pk_test_TwUTjFTZFArzA5EPS1q14xLA',
+    secret_key: 'sk_test_kjJOAy4BtVtxcjeDSutb3sh1'
   }
 end
 
@@ -14,9 +16,3 @@ end
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # Set your secret key: remember to change this to your live secret key in production
 # See your keys here: https://dashboard.stripe.com/account/apikeys
-
-    # publishable_key: ENV['stripe_publishable_key'],
-    # secret_key: ENV['stripe_secret_key']
-    # sk_test_kjJOAy4BtVtxcjeDSutb3sh1
-    # publishable_key: 'pk_test_TwUTjFTZFArzA5EPS1q14xLA',
-    # secret_key: 'pk_test_TwUTjFTZFArzA5EPS1q14xLA'
